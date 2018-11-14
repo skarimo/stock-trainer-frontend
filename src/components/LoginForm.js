b519bd09b11996a184f545bf8253b00278310796
skarimo <karimovj@gmail.com>
2018-11-14 10:55:10 -0500

    fixes and css update

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,33 +3,35 @@ import { Link } from 'react-router-dom'
 
 const LoginForm = (props) => {
     return (
-      <div className="column">
-          <h2 className="ui green image header">
-            {/*<img src="assets/images/logo.png" className="image" />*/}
-          </h2>
-          <form className="ui large form">
-            <div className="ui stacked segment" style={{backgroundColor: 'LightGrey'}}>
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="user icon"></i>
-                  <input onChange={props.handleLoginFormChange} type="text" name="username" placeholder="Username" />
+      <div className="background" style={{height: '100vh'}}>
+        <div className="column">
+            <h2 className="ui green image header">
+              {/*<img src="assets/images/logo.png" className="image" />*/}
+            </h2>
+            <form className="ui large form">
+              <div className="ui stacked segment" style={{backgroundColor: 'Silver'}}>
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="user icon"></i>
+                    <input onChange={props.handleLoginFormChange} type="text" name="username" placeholder="Username" />
+                  </div>
                 </div>
-              </div>
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="lock icon"></i>
-                  <input onChange={props.handleLoginFormChange} type="password" name="password" placeholder="Password" />
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="lock icon"></i>
+                    <input onChange={props.handleLoginFormChange} type="password" name="password" placeholder="Password" />
+                  </div>
                 </div>
+                <button onClick={(e) => props.handleLoginSubmit(e)} className="ui fluid large green submit button">Login</button>
               </div>
-              <button onClick={(e) => props.handleLoginSubmit(e)} className="ui fluid large green inverted submit button">Login</button>
+
+              <div className="ui error message"></div>
+
+            </form>
+
+            <div className="ui message" style={{backgroundColor: 'Silver'}}>
+              New to us? <Link to='/signup'>Sign Up</Link>
             </div>
-
-            <div className="ui error message"></div>
-
-          </form>
-
-          <div className="ui message" style={{backgroundColor: 'LightGrey'}}>
-            New to us? <Link to='/signup'>Sign Up</Link>
           </div>
         </div>
     )
