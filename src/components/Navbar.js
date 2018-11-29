ad82b2b313f0c0a9b1694de27a44953e444d969c
skarimo <karimovj@gmail.com>
2018-11-29 18:23:10 -0500

    heroku

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ class Navbar extends Component {
 
   getSearchResults = (searchObj) => {
     const token = localStorage.getItem("token")
-    fetch(`http://localhost:3000/api/v1/search`, {
+    fetch(`https://expense-logger-backend.herokuapp.com/api/v1/search`, {
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
